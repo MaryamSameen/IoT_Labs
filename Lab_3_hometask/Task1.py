2a4e0612d39f846d7456e91915c12fb7a8f3fa16
Maryam Sameen <maryamsameen16@gmail.com>
2025-02-27 10:26:11 +0500

    Added folder of Lab 3 Hometask and did Task 1

--- Lab_3_hometask/Task1.py
+++ Lab_3_hometask/Task1.py
@@ -1,36 +1,33 @@
-
 from machine import Pin, I2C, Timer
 import machine
-import ssd1306 
+import ssd1306
 import dht
 import time
 
 DHT_PIN = 4  # DHT22 data pin
 button = Pin(0, Pin.IN, Pin.PULL_UP)
+
 # Initialize DHT22 sensor
-dht_sensor = dht.DHT11(machine.Pin(DHT_PIN)) # change DHT11 fr physical device
+dht_sensor = dht.DHT11(machine.Pin(DHT_PIN))  # Change DHT11 for physical device
 
 # Initialize OLED display
 i2c = machine.I2C(scl=machine.Pin(9), sda=machine.Pin(8))
 oled = ssd1306.SSD1306_I2C(128, 64, i2c)
 
-pressed= False
+pressed = False
 debounce_timer = None
 
 def button_pressed(pin):
-    global  debounce_timer, pressed  # Declare variables as global
+    global debounce_timer, pressed  # Declare variables as global
 
     if debounce_timer is None:
-        pressed= not pressed
+        pressed = not pressed
         if pressed:
             oled.poweroff()
-           
         else:
             oled.poweron()
-            
-            
 
-        # Start a timer for debounce period (e.g., 200 milliseconds)
+        # Start a timer for debounce period (200ms)
         debounce_timer = Timer(0)
         debounce_timer.init(mode=Timer.ONE_SHOT, period=200, callback=debounce_callback)
 
@@ -43,25 +40,24 @@ button.irq(trigger=Pin.IRQ_FALLING, handler=button_pressed)
 
 
 
-
-
 # Main loop
 while True:
     try:
         dht_sensor.measure()
-        time.sleep(.2)
+        time.sleep(0.2)
         temp = dht_sensor.temperature()
         humidity = dht_sensor.humidity()
         print(temp, humidity)
+        
         oled.fill(0)
         oled.text("Temp: {} C".format(temp), 0, 0)
         oled.text("Humidity: {}%".format(humidity), 0, 16)
+        oled.text(":-)", 28, 30)
+        
         oled.show()
 
-
-
     except Exception as e:
-        print("Error reading DHT22 sensor:", e)
-    
-        
-    time.sleep(1)  # Update every 2 seconds
+        print("Error reading DHT11 sensor:", e)
+
+    time.sleep(1)  # Update every second
+
---
+++ Lab_3_hometask/readme.md
@@ -0,0 +1,8 @@
+### Task 1 : Blow on the sensor and observe whether it detects minor changes in temperature and humidity.
+
+Yes, It changes the temperature and humidity after a warm Blow on the sensor after a proper delay we have given it.
+
+-> Emojis does not get supported simply in esp, I tried using framebuff Library to display it as a Bitmap emoji but it didn't work, so I simply added Text emojis.
+
+### Task 2 : Running the Code Without Interrupt
+
